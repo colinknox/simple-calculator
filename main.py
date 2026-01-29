aa05d6aad559c8dec6d09f659e4d5c67f597a25b
colinknox <colinwknox@gmail.com>
2026-01-29 07:21:51 -0500

    added function to validate inputs as integers and floats for most public methods

--- main.py
+++ main.py
@@ -3,22 +3,41 @@ class Calculator:
         if self.__is_valid_number(a) and self.__is_valid_number(b):
             return a + b
         else:
+            print("ERROR: Please input either a integer or a float")
             return None
 
     def subtract(self, a, b):
-        return a - b
+        if self.__is_valid_number(a) and self.__is_valid_number(b):
+            return a - b
+        else:
+            print("ERROR: Please input either a integer or a float")
+            return None
     
     def multiply(self, a, b):
-        return a * b
+        if self.__is_valid_number(a) and self.__is_valid_number(b):
+            return a * b
+        else:
+            print("ERROR: Please input either a integer or a float")
+            return None
     
     def power(self, base, exponent):
-        return self.__multiply_repeatedly(base, exponent)
+        if self.__is_valid_number(base) and self.__is_valid_number(exponent):
+            return self.__multiply_repeatedly(base, exponent)
+        else:
+            print("ERROR: Please input either a integer or a float")
+            return None
     
     def factorial(self, n):
         factorial_value = 1
 
-        for i in range(5, 0, -1):
-            factorial_value *= i
+        if n == 0:
+            return 1
+        elif n >= 0:
+            for i in range(n, 0, -1):
+                factorial_value *= i
+        else:
+            print("ERROR: Please input a positive integer")
+            return None
 
         return factorial_value
     
@@ -38,16 +57,12 @@ class Calculator:
 
             
 
-
-
-
-
 my_calc = Calculator()
 
-# print(my_calc.add(12, 3))
+print(my_calc.add("a", 3))
 # print(my_calc.subtract(10, 6))
 # print(my_calc.multiply(23, 100))
-print(my_calc.power(3, 5))
-# print(my_calc.factorial(5))
+# print(my_calc.power(3, 5))
+# print(my_calc.factorial(-1))
 # print(my_calc._factorial_numbers(True))
 # print(my_calc._multiply_repeatedly(2, 3))
