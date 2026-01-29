1ecd23c21bc4408b1e142841ff49316f30b09671
colinknox <colinwknox@gmail.com>
2026-01-28 22:48:51 -0500

    utilized private methods inside public methods

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 class Calculator:
     def add(self, a, b):
-        return a + b
+        if self.__is_valid_number(a) and self.__is_valid_number(b):
+            return a + b
+        else:
+            return None
 
     def subtract(self, a, b):
         return a - b
@@ -9,7 +12,7 @@ class Calculator:
         return a * b
     
     def power(self, base, exponent):
-        return base ** exponent
+        return self.__multiply_repeatedly(base, exponent)
     
     def factorial(self, n):
         factorial_value = 1
@@ -19,17 +22,32 @@ class Calculator:
 
         return factorial_value
     
-    def __factorial_numbers(self, value):
+    def __is_valid_number(self, value):
         if type(value) == int or type(value) == float:
             return True
         else:
             return False
 
+    def __multiply_repeatedly(self, base, times):
+        total = 1
+        
+        for i in range(1, times + 1):
+            total *= base
+
+        return total
+
+            
+
+
+
+
+
 my_calc = Calculator()
 
-# print(my_calc.add(1, 3))
+# print(my_calc.add(12, 3))
 # print(my_calc.subtract(10, 6))
 # print(my_calc.multiply(23, 100))
-# print(my_calc.power(3, 5))
+print(my_calc.power(3, 5))
 # print(my_calc.factorial(5))
-print(my_calc._factorial_numbers(True))
+# print(my_calc._factorial_numbers(True))
+# print(my_calc._multiply_repeatedly(2, 3))
